ad0c7a5a8ae149ff4d4fd108629e9bd3bfb52bea
Marc Eichenseher <this@marceichenseher.de>
2024-07-08 18:35:27 +0200

    qa: Try to run GitHub Actions

--- ansible/qa.sh
+++ ansible/qa.sh
@@ -3,11 +3,14 @@
 set -euox pipefail
 
 start=`date +%s`
+script_dir=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
+cd ${script_dir}
 
 time yamllint . --strict --config-file .yamllint.yaml
 time ansible-playbook site.yaml --inventory development.ini.example --syntax-check
 time ansible-lint site.yaml --strict --config-file .ansible-lint
 #time molecule test
 
+cd -
 end=`date +%s`
 finished_in_seconds=$((end-start))
